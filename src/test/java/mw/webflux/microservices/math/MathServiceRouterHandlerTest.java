0027ebc09a98d996e4192d6eebbb47102d8c00ea
mariusz.wojcik <mariusz.wojcik@asseco.pl>
2021-06-05 22:01:22 +0200

    post request test

--- src/test/java/mw/webflux/microservices/math/MathServiceRouterHandlerTest.java
+++ src/test/java/mw/webflux/microservices/math/MathServiceRouterHandlerTest.java
@@ -2,16 +2,13 @@ package mw.webflux.microservices.math;
 
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
-import org.assertj.core.api.Fail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
-
 
 @SpringBootTest(classes = MathRouterFunctionConfiguration.class)
 @Import({MathRouterFunctionConfiguration.class})
@@ -45,7 +42,6 @@ class MathServiceRouterHandlerTest {
         StepVerifier.create(mathResponseMono).expectNextMatches(it -> it.getValue() == 81).verifyComplete();
     }
 
-
     @DisplayName("Should return multiplicity table for given number")
     @Test
     void shouldReturnMultiplicityTableForGivenNumber() {
@@ -53,21 +49,35 @@ class MathServiceRouterHandlerTest {
                                         .uri("/reactive-math/router/table/{input}", 7)
                                         .retrieve()
                                         .bodyToFlux(MathResponse.class)
-            .doOnNext((it)->log.info(String.valueOf(it.getValue())));
+                                        .doOnNext((it) -> log.info(String.valueOf(it.getValue())));
 
         StepVerifier.create(mathResponseFlux).expectNextCount(10).verifyComplete();
-     }
+    }
 
-     @DisplayName("Should return multiplicity table stream for given number")
-     @Test
-     void shouldReturnMultiplicityTableStreamForGivenNumber() {
-         var mathResponseFlux = webClient.get()
-                                         .uri("/reactive-math/router/table-stream/{input}", 7)
-                                         .retrieve()
-                                         .bodyToFlux(MathResponse.class)
-                                         .doOnNext((it)->log.info(String.valueOf(it.getValue())));
+    @DisplayName("Should return multiplicity table stream for given number")
+    @Test
+    void shouldReturnMultiplicityTableStreamForGivenNumber() {
+        var mathResponseFlux = webClient.get()
+                                        .uri("/reactive-math/router/table-stream/{input}", 7)
+                                        .retrieve()
+                                        .bodyToFlux(MathResponse.class)
+                                        .doOnNext((it) -> log.info(String.valueOf(it.getValue())));
 
-         StepVerifier.create(mathResponseFlux).expectNextCount(10).verifyComplete();
+        StepVerifier.create(mathResponseFlux).expectNextCount(10).verifyComplete();
 
-     }
+    }
+
+    @DisplayName("Should multiply given numbers")
+    @Test
+    void shouldMultiplyGivenNumbers() {
+        var mathResponseMono = webClient.post()
+                                        .uri("/reactive-math/router/multiply")
+                                        //if producer type use body(), else bodyValue()
+                                        .bodyValue(new MultiplicityRequest(7, 8))
+                                        .retrieve()
+                                        .bodyToMono(MathResponse.class)
+                                        .doOnNext(it -> log.info(String.valueOf(it.getValue())));
+
+        StepVerifier.create(mathResponseMono).expectNextCount(1).verifyComplete();
+    }
 }
