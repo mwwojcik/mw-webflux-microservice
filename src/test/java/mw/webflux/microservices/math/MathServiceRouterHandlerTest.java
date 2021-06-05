988751cadfb2bbcb1c53f4c1f3ff381379810d03
mariusz.wojcik <mariusz.wojcik@asseco.pl>
2021-06-05 21:09:05 +0200

    webclient flux test

--- src/test/java/mw/webflux/microservices/math/MathServiceRouterHandlerTest.java
+++ src/test/java/mw/webflux/microservices/math/MathServiceRouterHandlerTest.java
@@ -1,16 +1,21 @@
 package mw.webflux.microservices.math;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Fail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
 
 @SpringBootTest(classes = MathRouterFunctionConfiguration.class)
 @Import({MathRouterFunctionConfiguration.class})
+@Slf4j
 class MathServiceRouterHandlerTest {
 
     @Autowired
@@ -39,4 +44,17 @@ class MathServiceRouterHandlerTest {
         //we have to use Reactor Producer testing library - Step Verifier
         StepVerifier.create(mathResponseMono).expectNextMatches(it -> it.getValue() == 81).verifyComplete();
     }
+
+
+    @DisplayName("Should return multiplicity table for given number")
+    @Test
+    void shouldReturnMultiplicityTableForGivenNumber() {
+        var mathResponseFlux = webClient.get()
+                                        .uri("/reactive-math/router/table/{input}", 7)
+                                        .retrieve()
+                                        .bodyToFlux(MathResponse.class)
+            .doOnNext((it)->log.info(String.valueOf(it.getValue())));
+
+        StepVerifier.create(mathResponseFlux).expectNextCount(10).verifyComplete();
+     }
 }
